93aa64044ef201638a3291b6d13aa1aec34aa4a2
Waxiw92 <63607294+Waxiw92@users.noreply.github.com>
2020-10-04 23:57:31 +0300

    Добавил константы

---
+++ gallery.js
@@ -0,0 +1,8 @@
+ import galleryItems from "./gallery-items.js"
+
+ const galleryEl = document.querySelector(".js-gallery");
+ const modalEl = document.querySelector(".js-lightbox");
+ const overlayEl = document.querySelector(".lightbox__overlay");
+ const modalImgEl = document.querySelector(".lightbox__image");
+ const closeBtnEl = document.querySelector(".lightbox__button");
+
