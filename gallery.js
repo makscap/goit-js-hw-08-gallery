a24f5d50c726547599d429a4f7799fd5b9a0a819
Waxiw92 <63607294+Waxiw92@users.noreply.github.com>
2020-10-04 23:58:49 +0300

    Добавил модалку

--- gallery.js
+++ gallery.js
@@ -6,3 +6,59 @@
  const modalImgEl = document.querySelector(".lightbox__image");
  const closeBtnEl = document.querySelector(".lightbox__button");
 
+  const galleryListEl = createGalleryList(galleryItems);
+
+  galleryEl.insertAdjacentHTML("beforeend",galleryListEl);
+
+  galleryEl.addEventListener("click", onOpenModal);
+  closeBtnEl.addEventListener("click", onCloseModal);
+  overlayEl.addEventListener("click", onCloseModalOnOverlay);
+  
+  function createGalleryList(img) {
+   return  img.map(({original, description, preview}) => {
+     return `<li class="gallery__item">
+   <a
+     class="gallery__link"
+     href="${original}"
+   >
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </li>`}).join("");
+ }  
+
+function onOpenModal (event) {
+  event.preventDefault();
+  if(event.target.nodeName !== "IMG") { 
+    return
+   };
+    document.addEventListener("keydown", onCloseModalByEsc);
+
+   modalEl.classList.add("is-open");
+   modalImgEl.src = event.target.dataset.source;
+   modalImgEl.alt = event.target.alt;
+
+}
+
+function onCloseModal (event) {
+ document.removeEventListener("keydown", onCloseModalByEsc);
+ modalEl.classList.remove("is-open");
+  modalImgEl.src = "";
+   
+
+}
+function onCloseModalOnOverlay (event) {
+  if (event.currentTarget === event.target) {
+   onCloseModal();
+  }
+}
+
+function onCloseModalByEsc (event) {
+  if (event.code === "Escape") {
+    onCloseModal();
+  }
+}
